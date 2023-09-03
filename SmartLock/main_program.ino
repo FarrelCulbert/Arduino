6e92913a86a7e8651bffac69c6640ca0c4258f07
Farrel <101645434+FarrelCulbert@users.noreply.github.com>
2023-09-03 14:41:14 +0700

    main_program.ino

--- SmartLock/main_program.ino
+++ SmartLock/main_program.ino
@@ -1,30 +1,64 @@
-#include <SPI.h>
+#include <Servo.h>
 #include <MFRC522.h>
+#include <SPI.h>
 
 #define SS_PIN 10   // Pin Chip Select untuk modul RFID
 #define RST_PIN 9   // Pin Reset untuk modul RFID
+#define LED_PIN 13  // Pin LED built-in Arduino (Opsional)
+#define SERVO_PIN 2 // Pin untuk servo
 
 MFRC522 mfrc522(SS_PIN, RST_PIN); // Inisialisasi MFRC522
+Servo myservo; // Inisialisasi servo
+
+// Daftar ID kartu yang diizinkan
+char* allowedCardIDs[] = {
+  "XXXXXXXXXX", // Ganti dengan ID kartu yang diizinkan
+  "YYYYYYYYYY"  // Ganti dengan ID kartu lainnya jika diperlukan
+};
 
 void setup() {
   Serial.begin(9600); // Inisialisasi komunikasi serial
   SPI.begin();        // Inisialisasi SPI bus
   mfrc522.PCD_Init(); // Inisialisasi modul RFID
-  Serial.println("Siap untuk membaca kartu RFID...");
+
+  myservo.attach(SERVO_PIN); // Menghubungkan servo ke pin 2
+  myservo.write(0);          // Menggerakkan servo ke posisi awal (0 derajat)
+  delay(1000);
+  digitalWrite(LED_PIN, LOW); // Matikan LED (Opsional)
 }
 
 void loop() {
   // Cek apakah ada tag RFID yang terdeteksi
   if (mfrc522.PICC_IsNewCardPresent() && mfrc522.PICC_ReadCardSerial()) {
-    Serial.print("ID Kartu: ");
+    Serial.println("Tag RFID Terdeteksi!");
 
-    // Baca dan tampilkan ID kartu dalam bentuk hexadecimal
+    // Baca ID kartu dari tag RFID
+    String cardID = "";
     for (byte i = 0; i < mfrc522.uid.size; i++) {
-      Serial.print(mfrc522.uid.uidByte[i] < 0x10 ? " 0" : " ");
-      Serial.print(mfrc522.uid.uidByte[i], HEX);
+      cardID += String(mfrc522.uid.uidByte[i] < 0x10 ? "0" : "");
+      cardID += String(mfrc522.uid.uidByte[i], HEX);
     }
-    
-    Serial.println(); // Pindah ke baris baru
+
+    // Periksa apakah ID kartu ada dalam daftar yang diizinkan
+    if (isCardAllowed(cardID)) {
+      Serial.println("Akses Diberikan!");
+      myservo.write(90); // Menggerakkan servo ke posisi 90 derajat
+      delay(1000);       // Tunggu selama 1 detik
+      myservo.write(0);  // Mengembalikan servo ke posisi awal (0 derajat)
+    } else {
+      Serial.println("Akses Ditolak!");
+    }
+
     mfrc522.PICC_HaltA(); // Menonaktifkan tag RFID saat selesai
   }
+}
+
+bool isCardAllowed(String cardID) {
+  // Memeriksa apakah ID kartu ada dalam daftar yang diizinkan
+  for (int i = 0; i < sizeof(allowedCardIDs) / sizeof(allowedCardIDs[0]); i++) {
+    if (cardID == allowedCardIDs[i]) {
+      return true;
+    }
+  }
+  return false;
 }
